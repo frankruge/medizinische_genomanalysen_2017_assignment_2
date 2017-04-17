f7c58c462b613ffd03cc8833cc571dc5539e3a78
frankruge <frankruge@gmail.com>
2017-04-17 23:12:13 +0200

    update

--- assignment2.py
+++ assignment2.py
@@ -14,32 +14,32 @@ class Assignment2:
         ## Check if pyvcf is installed
         print("PyVCF version: %s" % vcf.VERSION)
         self.vcf = vcf.Reader(filename="AmpliseqExome.20141120.NA24385.vcf")
+        self.header = self.vcf._header_lines
+        self.records = list([i for i in self.vcf])
 
-
-    @property
     def get_average_quality_of_son(self):
-        avg_PHRED= np.mean([i.QUAL for i in self.vcf])
+        avg_PHRED= np.mean([i.QUAL for i in self.records])
         #1753.77822224
         return avg_PHRED
         
         
     def get_total_number_of_variants_of_son(self):
         count=0
-        for record in self.vcf:
+        for record in self.records:
             count+=1
         return count #38526
 
     
     
     def get_variant_caller_of_vcf(self):
-        for record in self.vcf._header_lines:
+        for record in self.header:
             if "##source=" in record:
                 caller=record
             exit
         return(caller.lstrip("##source="))
         
     def get_human_reference_version(self):
-        for record in self.vcf._header_lines:
+        for record in self.header:
             if "##reference=" in record:
                 caller=record
             exit
@@ -48,7 +48,7 @@ class Assignment2:
         
     def get_number_of_indels(self):
         count=0
-        for record in self.vcf:
+        for record in self.records:
             if record.is_indel:
                 count+=1
         return count
@@ -56,14 +56,14 @@ class Assignment2:
 
     def get_number_of_snvs(self):
         count = 0
-        for record in self.vcf:
+        for record in self.records:
             if record.is_snp:
                 count += 1
         return count
         
     def get_number_of_heterozygous_variants(self):
         count = 0
-        for record in self.vcf:
+        for record in self.records:
             if record.heterozygosity:
                 count += 1
         return count
@@ -71,12 +71,12 @@ class Assignment2:
         
     
     def print_summary(self):
-        #avg_PHRED = assignment1.get_average_quality_of_son(); print(avg_PHRED)
-        #number_var = assignment1.get_total_number_of_variants_of_son(); print(number_var)
-        #variant_caller = assignment1.get_variant_caller_of_vcf(); print("Variant Caller: "+variant_caller)
-        #reference_version = assignment1.get_human_reference_version(); print("Reference used:"+reference_version)
-        #number_indels=assignment1.get_number_of_indels(); print("Number of Indels: "+str(number_indels))
-        #snv=assignment1.get_number_of_snvs();print("SNPs including SNVs: "+str(snv))
+        avg_PHRED = assignment1.get_average_quality_of_son(); print(avg_PHRED)
+        number_var = assignment1.get_total_number_of_variants_of_son(); print(number_var)
+        variant_caller = assignment1.get_variant_caller_of_vcf(); print("Variant Caller: "+variant_caller)
+        reference_version = assignment1.get_human_reference_version(); print("Reference used:"+reference_version)
+        number_indels=assignment1.get_number_of_indels(); print("Number of Indels: "+str(number_indels))
+        snv=assignment1.get_number_of_snvs();print("SNPs including SNVs: "+str(snv))
         heterozygous_variants = assignment1.get_number_of_heterozygous_variants();print("Heterozygous variants: "+str(heterozygous_variants))
         
 if __name__ == '__main__':
